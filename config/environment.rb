66ab4d43696f76abddd9c87626e90defaf3d85c0
Bruno Gauglitz <bruno.gauglitz@gmail.com>
2016-10-23 00:33:16 -0200

    Remove misplaced scrutinizer code

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-build:
-    tests:
-        override:
-            -
-                command: 'bundle exec rspec spec'
-                environment:
-                    'SCRUTINIZER_CC_FILE': 'my-coverage'
-                coverage:
-                    file: 'my-coverage'
-                    format: 'rb-cc'
